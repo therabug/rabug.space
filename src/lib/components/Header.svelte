0e53868feb0b5be38a9a0a1985a6b6148b78f1cd
therabug <smallhacker709@gmail.com>
2026-04-19 15:53:29 +0300

    added games link and fixed padding issues

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Book, Folder, Headset, House, Icon } from 'lucide-svelte';
+	import { Book, Folder, Headset, House, Icon, Gamepad } from 'lucide-svelte';
 	import { page } from '$app/state';
 
 	interface Page {
@@ -24,11 +24,22 @@
 </script>
 
 <header
-	class="fixed top-0 z-50 hidden min-w-screen items-center justify-center border-b border-dark-3 bg-dark-5 p-4 select-none md:flex"
+	class="fixed top-0 z-50 hidden min-w-screen items-center justify-between border-b border-dark-3 bg-dark-5 p-4 select-none md:flex"
 >
+	<div>
+		<a
+			draggable={false}
+			href="https://apps.rabug.space"
+			class="flex aspect-square h-full items-center gap-2 rounded-lg bg-primary p-2 transition-all duration-300 ease-out hover:bg-purple-7 active:bg-purple-5"
+		>
+			<Gamepad></Gamepad>
+		</a>
+	</div>
+
 	<div class="flex gap-2">
 		{#each navItems as item (item.label)}
 			<a
+				draggable={false}
 				href={item.path}
 				class={`flex items-center gap-2 rounded-lg p-2 px-8 text-lg transition-all duration-300 ease-out hover:bg-dark-1 active:bg-dark-2 ${isActive(item.path) ? 'bg-dark-3' : ''}`}
 			>
@@ -37,6 +48,8 @@
 			</a>
 		{/each}
 	</div>
+
+	<div></div>
 </header>
 
 <nav
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,7 @@
 		SiSvelte,
 		SiUnrealengine
 	} from '@icons-pack/svelte-simple-icons';
+	import { Gamepad } from 'lucide-svelte';
 	import { onDestroy } from 'svelte';
 	const helloList = [
 		'Hello!',
@@ -88,54 +89,67 @@
 	});
 </script>
 
-<section class="flex flex-col items-center justify-center gap-4 p-4 py-0">
-	<div
-		class="mt-8 flex min-h-[1.2em] items-center justify-center text-6xl font-bold select-none md:mt-0 md:text-9xl"
-	>
-		<h1 class="hello">
-			{displayedText}
-		</h1>
-		<span class="cursor">|</span>
-	</div>
-
-	<div class="mt-8 flex flex-col gap-4 lg:px-64">
-		<p class="text-md mb-2 text-center md:text-xl">
-			<span class="rounded-2xl border-2 border-white/20 bg-dark-5 p-3"
-				>I'm Buğra! I'm
-				{@html `<!--get dunked this is what you receive from the server. no JS needed, no hydration, just pure SSR. birthdate? what birthdate? i don't know her.-->`}
-				<span class="font-bold text-primary">{data.age.years}</span>
-				years old.</span
-			>
-		</p>
-		<p class="rounded-3xl border-2 border-white/20 bg-dark-5 p-4 text-lg md:rounded-xl lg:text-xl">
-			Also known as rabug, I started coding at age 9 with C#. Still, I love programming, and my goal
-			is to be successful. I live in Turkiye 🇹🇷. I studied English in the English project class for
-			4 years straight. I don't believe coding requires math because solving a math problem is a
-			computer job, and I find this funny. I also make animations, 3D models, games, and design.
-			Anyway, if you want to contact me or something, you can look at the <a href="/contact"
-				>contact</a
-			> page.
-		</p>
-	</div>
-</section>
-
-<section class="flex flex-col items-center justify-center gap-4 p-4">
-	<div class="mt-8 flex flex-col gap-4 lg:px-64">
-		<p class="text-md mb-2 text-center md:text-xl">
-			<span class="rounded-2xl border-2 border-white/20 bg-dark-5 p-3 text-2xl">Tech Stack</span>
-		</p>
+<div class="p-4 md:px-32 lg:px-64">
+	<section class="flex flex-col items-center justify-center gap-4 py-0">
 		<div
-			class="grid grid-cols-2 rounded-3xl border-2 border-white/20 bg-dark-5 text-lg sm:grid-cols-3 md:rounded-xl lg:text-xl xl:grid-cols-4"
+			class="mt-8 flex min-h-[1.2em] items-center justify-center text-6xl font-bold select-none md:mt-0 md:text-9xl"
 		>
-			{#each techStack as tech (tech.name)}
-				<div class="flex flex-col items-center justify-center gap-2 p-4">
-					<tech.icon size={48} />
-					<p class="text-center text-gray-400">{tech.name}</p>
-				</div>
-			{/each}
+			<h1 class="hello">
+				{displayedText}
+			</h1>
+			<span class="cursor">|</span>
 		</div>
-	</div>
-</section>
+
+		<div class="mt-8 flex flex-col gap-4">
+			<p class="text-md mb-2 text-center md:text-xl">
+				<span class="rounded-2xl border-2 border-white/20 bg-dark-5 p-3"
+					>I'm Buğra! I'm
+					{@html `<!--get dunked this is what you receive from the server. no JS needed, no hydration, just pure SSR. birthdate? what birthdate? i don't know her.-->`}
+					<span class="font-bold text-primary">{data.age.years}</span>
+					years old.</span
+				>
+			</p>
+			<p
+				class="rounded-3xl border-2 border-white/20 bg-dark-5 p-4 text-lg md:rounded-xl lg:text-xl"
+			>
+				Also known as rabug, I started coding at age 9 with C#. Still, I love programming, and my
+				goal is to be successful. I live in Turkiye 🇹🇷. I studied English in the English project
+				class for 4 years straight. I don't believe coding requires math because solving a math
+				problem is a computer job, and I find this funny. I also make animations, 3D models, games,
+				and design. Anyway, if you want to contact me or something, you can look at the <a
+					href="/contact">contact</a
+				> page.
+			</p>
+		</div>
+	</section>
+
+	<section class="flex flex-col items-center justify-center gap-4 py-4">
+		<div class="mt-8 flex w-full flex-col gap-4">
+			<p class="text-md mb-2 text-center md:text-xl">
+				<span class="rounded-2xl border-2 border-white/20 bg-dark-5 p-3 text-2xl">Tech Stack</span>
+			</p>
+			<div
+				class="grid grid-cols-2 rounded-3xl border-2 border-white/20 bg-dark-5 text-lg sm:grid-cols-3 md:rounded-xl lg:text-xl xl:grid-cols-4"
+			>
+				{#each techStack as tech (tech.name)}
+					<div class="flex flex-col items-center justify-center gap-2 p-4">
+						<tech.icon size={48} />
+						<p class="text-center text-gray-400">{tech.name}</p>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</section>
+
+	<a
+		draggable={false}
+		href="https://apps.rabug.space"
+		class="flex h-20 items-center justify-between gap-2 rounded-lg bg-primary p-4 text-xl text-white! decoration-0! transition-all duration-300 ease-out hover:bg-purple-7 active:bg-purple-5 md:hidden"
+	>
+		<Gamepad size={30}></Gamepad> Check out at apps & games!
+		<p></p>
+	</a>
+</div>
 
 <style>
 	.hello {
